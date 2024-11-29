8d4551f5bd5a4995251651b5e61518ae7fb57996
AdenierOsorto <adenierosorto@gmail.com>
2024-11-29 14:06:29 -0600

    added new function to create a new context logger

--- v3/logger_config.go
+++ v3/logger_config.go
@@ -7,7 +7,7 @@ import (
 	"github.com/google/uuid"
 )
 
-func SetLoggerConfig(ctx context.Context, taskName string) context.Context {
+func NewContextLogger(ctx context.Context, taskName string) context.Context {
 	var keyCtx ContextKey = "context-data"
 	contextData := CtxKeys{
 		TaskName:  taskName,
